110683701faea9b2503b35260d6d6fc41864f7a5
alamin1x0 <msasakibalamin1@gmail.com>
2023-05-15 11:52:19 +0600

    Cuvefo

---
+++ android/app/src/main/kotlin/com/vebto/bedrive/Application.kt
@@ -0,0 +1,11 @@
+package com.cuvefo.apps
+
+import io.flutter.app.FlutterApplication
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
+
+class Application : FlutterApplication(), PluginRegistrantCallback {
+    override fun registerWith(registry: PluginRegistry) {
+
+    }
+}
